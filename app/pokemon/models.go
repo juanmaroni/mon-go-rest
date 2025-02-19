fcf42b77f3cb213696c5225f833ba36e190b690d
Juanma Romero <pro@juanmaroni.me>
2025-02-19 11:34:19 +0100

    Pokémon model

---
+++ app/pokemon/models.go
@@ -0,0 +1,8 @@
+package pokemon
+
+type Pokemon struct {
+	Id int `json:"id" bson:"_id"`
+	Name string `json:"name" bson:"name"`
+	Type1 string `json:"type1" bson:"type1"`
+	Type2 string `json:"type2" bson:"type2"`
+}
