953abdd3941cebda3d67864911caf6aa2aa903d7
Juanma Romero <pro@juanmaroni.me>
2025-03-05 19:07:24 +0100

    More errors

--- app/handlers/errors/errors.go
+++ app/handlers/errors/errors.go
@@ -2,14 +2,26 @@ package errors
 
 import "net/http"
 
-// HTTP Error 500
-func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusInternalServerError)
-    w.Write([]byte("500 Internal Server Error"))
+// HTTP Error 403
+func ForbiddenHandler(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusNotFound)
+    w.Write([]byte("403 Forbidden"))
 }
 
 // HTTP Error 404
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusNotFound)
     w.Write([]byte("404 Not Found"))
+}
+
+// HTTP Error 405
+func MethodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusNotFound)
+    w.Write([]byte("405 Method Not Allowed"))
+}
+
+// HTTP Error 500
+func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("500 Internal Server Error"))
 }
