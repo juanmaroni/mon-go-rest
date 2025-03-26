3abb190d0c470d3536ba7e3b0b01a01de143d198
Juanma Romero <pro@juanmaroni.me>
2025-03-26 19:13:42 +0100

    Returning HTTP error messages for logging

--- app/handlers/httperrors/httperrors.go
+++ app/handlers/httperrors/httperrors.go
@@ -3,25 +3,37 @@ package httperrors
 import "net/http"
 
 // HTTP Error 403
-func ForbiddenHandler(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusNotFound)
-    w.Write([]byte("403 Forbidden"))
+func ForbiddenHandler(w http.ResponseWriter, r *http.Request) []byte {
+	msg := []byte("403 Forbidden")
+    w.WriteHeader(http.StatusForbidden)
+    w.Write(msg)
+
+	return msg
 }
 
 // HTTP Error 404
-func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) []byte {
+	msg := []byte("404 Not Found")
     w.WriteHeader(http.StatusNotFound)
-    w.Write([]byte("404 Not Found"))
+    w.Write(msg)
+
+	return msg
 }
 
 // HTTP Error 405
-func MethodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusNotFound)
-    w.Write([]byte("405 Method Not Allowed"))
+func MethodNotAllowedHandler(w http.ResponseWriter, r *http.Request) []byte {
+	msg := []byte("405 Method Not Allowed")
+    w.WriteHeader(http.StatusMethodNotAllowed)
+    w.Write(msg)
+
+	return msg
 }
 
 // HTTP Error 500
-func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
+func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) []byte {
+	msg := []byte("500 Internal Server Error")
     w.WriteHeader(http.StatusInternalServerError)
-    w.Write([]byte("500 Internal Server Error"))
+    w.Write(msg)
+
+	return msg
 }
