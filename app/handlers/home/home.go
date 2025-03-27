c8d45ee983aff19a71ca9c3aeed9a8e9d27a6aaf
Juanma Romero <pro@juanmaroni.me>
2025-03-27 18:35:14 +0100

    Minor fix

--- app/handlers/home/home.go
+++ app/handlers/home/home.go
@@ -5,5 +5,6 @@ import "net/http"
 type HomeHandler struct {}
 
 func (h *HomeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("PokéAPI Mini home page."))
 }
